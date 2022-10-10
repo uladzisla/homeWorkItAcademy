ce182a2c9fa20bad0896f813b368d91393141057
Vladislav <k-vladislav-v@mail.ru>
2022-10-10 22:45:13 +0300

    ++second task  complete

--- modules/HW3Classes/task2.js
+++ modules/HW3Classes/task2.js
@@ -3,3 +3,14 @@ String.prototype.reverse = function (str) {
 };
 let test = "test";
 console.log(test.reverse());
+
+String.prototype.ucFirst = function (str) {
+  return this[0].toUpperCase() + this.slice(1);
+};
+console.log(test.ucFirst());
+
+// String.prototype.ucWords = function (str) {
+//   return this.split("").map(item =>{
+//     if(item == ' ')
+//   });
+// };
